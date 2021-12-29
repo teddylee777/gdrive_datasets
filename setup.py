f870e7f170874b7099110848d969d2532a7ab223
teddylee777 <teddylee777@gmail.com>
2021-12-29 12:28:12 +0900

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdrive-dataset',
-    version='0.0.2',
+    version='0.0.3',
     description='easy downloader for google drive datasets',
     author='teddylee777',
     author_email='teddylee777@gmail.com',
